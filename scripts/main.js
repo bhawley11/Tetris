2cec9e67a1073e6e79259979a5e73981258d8d9d
bhawley11 <bhawley11@gmail.com>
2015-03-17 22:26:09 -0600

    Added a main script that will hold initialize and logic. Completed the loader and modernizr scripts (unless something else needs to be loaded). Linked the css in the index.html. Added the canvas

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created on 3/17/2015.
+ */
+
+TETRIS.initialize = (function() {
+
+    /****************************
+     *      INITIALIZATION      *
+     ***************************/
+
+    return function() {
+        console.log('Game Initializing...');
+
+    };
+}());
